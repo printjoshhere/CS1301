39dcee0a68e475a743767c30b9f54051bf2ea6db
Joshua Lu <joshualu888@gmail.com>
2021-06-10 22:19:20 -0500

    Static Method

--- PythonLearning1.py
+++ PythonLearning1.py
@@ -1,17 +1,22 @@
 # Class Attributes, Methods, and Properties 1
 class ShippingContainer:
-    # ATTRIBUTE
+
     next_serial = 1337
+
+    def _generate_serial():
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        # We have to "call" the class by name or we will get local variable error
-        self.serial = ShippingContainer.next_serial # self.serial = self.next_serial (Avoid this - not as clear)
-        ShippingContainer.next_serial += 1
-
+        self.serial = ShippingContainer._generate_serial()
+       
 c4 = ShippingContainer("ESC", ["Electronics"])
 print(c4.serial)
 
 c5 = ShippingContainer("ESC", ["Pharmaceuticals"])
 print(c5.serial)
+
+print(ShippingContainer.next_serial)
